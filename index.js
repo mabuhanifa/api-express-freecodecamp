8aafd83a38ea993605f0121eddc5cefecfd005fc
shourovahmd <moh.abuhanifa@gmail.com>
2023-02-13 23:34:51 +0600

    first

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const cors = require('cors');
+const port = process.env.PORT ||5000;
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+app.listen(port,()=>{
+    console.log(`App listening on ${port}`);
+});
