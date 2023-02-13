82253b907100756edfbda35e835f6f07cbc7afe4
shourovahmd <moh.abuhanifa@gmail.com>
2023-02-14 01:38:57 +0600

    first api

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
-const express = require('express');
-const cors = require('cors');
-const port = process.env.PORT ||5000;
+const express = require("express");
+const cors = require("cors");
+const port = process.env.PORT || 5000;
 const app = express();
 app.use(express.json());
 app.use(cors());
 
-app.listen(port,()=>{
-    console.log(`App listening on ${port}`);
+app.get("/", (req, res) => {
+  res.send("application data from get request");
+});
+
+app.listen(port, () => {
+  console.log(`App listening on ${port}`);
 });
