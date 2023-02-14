d6c90cb83493571cc2eaae62898350c80847ca3f
shourovahmd <moh.abuhanifa@gmail.com>
2023-02-15 00:02:32 +0600

    dependencies

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const port = process.env.PORT || 5000;
 const app = express();
 app.use(express.json());
 app.use(cors());
+const jwt = require("jsonwebtoken");
 
 app.get("/", (req, res) => {
   res.send("application data from get request");
